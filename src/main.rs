31f4bf26b7f802aed6557769a96e6960b1ba6341
xuhaojie <xuhaojie@hotmail.com>
2022-09-27 17:01:36 +0800

    支持从文件读取响应

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 // to create a self-signed temporary cert for testing: `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
 
-use std::{io::BufReader, fs::File};
+use std::{io::BufReader, fs::{File, self}, cell::Cell, sync::Mutex};
+#[cfg(feature="with_rustls")]
 use rustls::{Certificate, PrivateKey, ServerConfig};
+#[cfg(feature="with_rustls")]
 use rustls_pemfile::{certs, pkcs8_private_keys};
 
 use actix_web::{web::{self}, App, HttpServer, HttpResponse, http::header};
@@ -17,6 +19,53 @@ const DEFAULT_PORT : u16 = 3000;
 const DEFAULT_KEY_FILE : &str= "key.pem";
 const DEFAULT_CERT_FILE : &str= "cert.pem";
 const DEFAULT_CONNECTIONS : usize = 25*1024;
+
+mod response;
+
+#[derive(Clone)]
+pub struct ResponseData {
+	pub index: String,
+	pub get: String,
+	pub post: String,
+	pub put: String,
+	pub delete: String,
+}
+
+impl ResponseData {
+	fn new() -> Self {
+		ResponseData{
+			index: response::INDEX_RESPONSE.to_string(),
+			get: response::GET_RESPONSE.to_string(),
+			post: response::POST_RESPONSE.to_string(),
+			put: response::PUT_RESPONSE.to_string(),
+			delete: response::DELETE_RESPONSE.to_string(),
+		}
+	}
+
+	fn load() -> Self {
+		let index =  match std::fs::read_to_string("index.html"){
+			std::result::Result::Ok(text) => text,
+			_ => response::INDEX_RESPONSE.to_string(),
+		};		
+		let get =  match std::fs::read_to_string("get.json"){
+			std::result::Result::Ok(text) => text,
+			_ => response::GET_RESPONSE.to_string(),
+		};
+		let post =  match std::fs::read_to_string("post.json"){
+			std::result::Result::Ok(text) => text,
+			_ => response::GET_RESPONSE.to_string(),
+		};
+		let put =  match std::fs::read_to_string("put.json"){
+			std::result::Result::Ok(text) => text,
+			_ => response::GET_RESPONSE.to_string(),
+		};	
+		let delete =  match std::fs::read_to_string("delete.json"){
+			std::result::Result::Ok(text) => text,
+			_ => response::GET_RESPONSE.to_string(),
+		};				
+		ResponseData{index, get, post, put, delete}
+	}	
+}
 
 #[actix_web::main]
 async fn main() -> Result<()> {
@@ -37,8 +86,7 @@ async fn main() -> Result<()> {
 							.value_name("cert")
 							.takes_value(true)
 						  	.help("Certificate chain file, default cert.pem, env key: CERT_FILE"))
-					
-						  .arg(Arg::with_name("ip")
+ 						  .arg(Arg::with_name("ip")
 						  	.short('i')
 							.value_name("ip")
 							.takes_value(true)
@@ -123,9 +171,11 @@ async fn main() -> Result<()> {
 		}
 	};
 
-	let mut server = HttpServer::new(|| App::new()
-	.configure(server_routes)
-	.configure(benchmark_routes));
+	let response_data =web::Data::new(ResponseData::load());
+
+	let mut server = HttpServer::new(move || App::new()
+	.app_data(response_data.clone())
+	.configure(config_routes));
 	
 	if workers > 0 	{
 		info!("set server workers to {}", workers);
@@ -154,7 +204,7 @@ async fn main() -> Result<()> {
 			builder.set_certificate_chain_file(cert_file_name)?;
 			server.bind_openssl(https_address, builder)?.run().await?;
 		}
-		#[cfg(feature = "with_rustls")]
+		#[cfg(not(feature = "with_openssl"))]
 		{
 			info!("https server build with rustls");	
 			let cert_file = &mut BufReader::new(File::open(cert_file_name)?);
@@ -178,36 +228,7 @@ async fn main() -> Result<()> {
 
 }
 
-
-pub fn server_routes(cfg: &mut web::ServiceConfig) {
-	cfg
-	.route("/", web::get().to(index));
-}
-
-pub async fn index() -> HttpResponse  {
-	HttpResponse::Ok()
-	.insert_header(header::ContentType(mime::TEXT_HTML_UTF_8))
-	.body(
-r#"<!DOCTYPE html>
-<html>
-<head>
-	<meta charset='utf-8'>
-	<title>HTTP(S) Benchmark Server</title>
-	<link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-	<script src='main.js'></script>
-</head>
-<body>
-	
-	<div style="text-align:center;">
-		<H1>HTTP(S) Benchmark Server</H1>
-	</div>
-</body>
-</html>"#
-	)
-}
-
-
-pub fn benchmark_routes(cfg: &mut web::ServiceConfig) {
+pub fn config_routes(cfg: &mut web::ServiceConfig) {
 	cfg
 	.route("/", web::get().to(index))
 	.route("/get", web::get().to(bench_get))
@@ -216,101 +237,32 @@ pub fn benchmark_routes(cfg: &mut web::ServiceConfig) {
 	.route("/delete", web::delete().to(bench_delete));
 }
 
-
-pub async fn bench_get() -> HttpResponse  {
+pub async fn index(data: web::Data<ResponseData>) -> HttpResponse  {
 	HttpResponse::Ok()
-	.insert_header(header::ContentType(mime::APPLICATION_JSON))
-	.body(
-r#"{
-"args": {},
-"headers": {
-	"Accept": "application/json",
-	"Accept-Encoding": "gzip, deflate",
-	"Accept-Language": "en-US,en;q=0.5",
-	"Host": "www.httpbin.org",
-	"Referer": "http://www.httpbin.org/",
-	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:104.0) Gecko/20100101 Firefox/104.0",
-	"X-Amzn-Trace-Id": "Root=1-632dce82-279a47540dd200b652a8cb02"
-},
-"origin": "113.200.214.222",
-"url": "http://www.httpbin.org/get"
-}"#)
+	.insert_header(header::ContentType(mime::TEXT_HTML_UTF_8))
+	.body(data.index.clone())
 }
 
-pub async fn bench_post() -> HttpResponse  {
+pub async fn bench_get(data: web::Data<ResponseData>) -> HttpResponse  {
 	HttpResponse::Ok()
 	.insert_header(header::ContentType(mime::APPLICATION_JSON))
-	.body(
-r#"{
-	"args": {},
-	"data": "",
-	"files": {},
-	"form": {},
-	"headers": {
-		"Accept": "application/json",
-		"Accept-Encoding": "gzip, deflate",
-		"Accept-Language": "zh-cn",
-		"Content-Length": "0",
-		"Host": "httpbin.org",
-		"Origin": "http://httpbin.org",
-		"Referer": "http://httpbin.org/",
-		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
-		"X-Amzn-Trace-Id": "Root=1-632dd138-2243642a76ba30163e857a96"
-	},
-	"json": null,
-	"origin": "113.200.214.222",
-	"url": "http://httpbin.org/put"
-	}"#)
+	.body(data.get.clone())
 }
 
-pub async fn bench_put() -> HttpResponse  {
+pub async fn bench_post(data: web::Data<ResponseData>) -> HttpResponse  {
 	HttpResponse::Ok()
 	.insert_header(header::ContentType(mime::APPLICATION_JSON))
-	.body(
-r#"{
-	"args": {},
-	"data": "",
-	"files": {},
-	"form": {},
-	"headers": {
-		"Accept": "application/json",
-		"Accept-Encoding": "gzip, deflate",
-		"Accept-Language": "zh-cn",
-		"Content-Length": "0",
-		"Host": "httpbin.org",
-		"Origin": "http://httpbin.org",
-		"Referer": "http://httpbin.org/",
-		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
-		"X-Amzn-Trace-Id": "Root=1-632dd138-2243642a76ba30163e857a96"
-	},
-	"json": null,
-	"origin": "113.200.214.222",
-	"url": "http://httpbin.org/put"
-	}"#)
+	.body(data.post.clone())
 }
 
-pub async fn bench_delete() -> HttpResponse  {
+pub async fn bench_put(data: web::Data<ResponseData>) -> HttpResponse  {
 	HttpResponse::Ok()
 	.insert_header(header::ContentType(mime::APPLICATION_JSON))
-	.body(
-r#"{
-	"args": {},
-	"data": "",
-	"files": {},
-	"form": {},
-	"headers": {
-		"Accept": "application/json",
-		"Accept-Encoding": "gzip, deflate",
-		"Accept-Language": "zh-cn",
-		"Content-Length": "0",
-		"Host": "httpbin.org",
-		"Origin": "http://httpbin.org",
-		"Referer": "http://httpbin.org/",
-		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
-		"X-Amzn-Trace-Id": "Root=1-632dd138-2243642a76ba30163e857a96"
-	},
-	"json": null,
-	"origin": "113.200.214.222",
-	"url": "http://httpbin.org/put"
-	}"#)
+	.body(data.put.clone())
+}
+
+pub async fn bench_delete(data: web::Data<ResponseData>) -> HttpResponse  {
+	HttpResponse::Ok()
+	.insert_header(header::ContentType(mime::APPLICATION_JSON))
+	.body(data.delete.clone())
 }
---
+++ src/response.rs
@@ -0,0 +1,99 @@
+pub const INDEX_RESPONSE: &str =
+r#"<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<title>HTTP(S) Benchmark Server</title>
+	<link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+	<script src='main.js'></script>
+</head>
+<body>
+	
+	<div style="text-align:center;">
+		<H1>HTTP(S) Benchmark Server</H1>
+	</div>
+</body>
+</html>"#;
+
+	
+pub const GET_RESPONSE: &str =
+r#"{
+"args": {},
+"headers": {
+	"Accept": "application/json",
+	"Accept-Encoding": "gzip, deflate",
+	"Accept-Language": "en-US,en;q=0.5",
+	"Host": "www.httpbin.org",
+	"Referer": "http://www.httpbin.org/",
+	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:104.0) Gecko/20100101 Firefox/104.0",
+	"X-Amzn-Trace-Id": "Root=1-632dce82-279a47540dd200b652a8cb02"
+},
+"origin": "113.200.214.222",
+"url": "http://www.httpbin.org/get"
+}"#;
+
+pub const POST_RESPONSE: &str =
+r#"{
+	"args": {},
+	"data": "",
+	"files": {},
+	"form": {},
+	"headers": {
+		"Accept": "application/json",
+		"Accept-Encoding": "gzip, deflate",
+		"Accept-Language": "zh-cn",
+		"Content-Length": "0",
+		"Host": "httpbin.org",
+		"Origin": "http://httpbin.org",
+		"Referer": "http://httpbin.org/",
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+		"X-Amzn-Trace-Id": "Root=1-632dd138-2243642a76ba30163e857a96"
+	},
+	"json": null,
+	"origin": "113.200.214.222",
+	"url": "http://httpbin.org/put"
+}"#;
+
+pub const PUT_RESPONSE: &str =
+r#"{
+	"args": {},
+	"data": "",
+	"files": {},
+	"form": {},
+	"headers": {
+		"Accept": "application/json",
+		"Accept-Encoding": "gzip, deflate",
+		"Accept-Language": "zh-cn",
+		"Content-Length": "0",
+		"Host": "httpbin.org",
+		"Origin": "http://httpbin.org",
+		"Referer": "http://httpbin.org/",
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+		"X-Amzn-Trace-Id": "Root=1-632dd138-2243642a76ba30163e857a96"
+	},
+	"json": null,
+	"origin": "113.200.214.222",
+	"url": "http://httpbin.org/put"
+}"#;
+
+pub const DELETE_RESPONSE: &str =
+r#"{
+	"args": {},
+	"data": "",
+	"files": {},
+	"form": {},
+	"headers": {
+		"Accept": "application/json",
+		"Accept-Encoding": "gzip, deflate",
+		"Accept-Language": "zh-cn",
+		"Content-Length": "0",
+		"Host": "httpbin.org",
+		"Origin": "http://httpbin.org",
+		"Referer": "http://httpbin.org/",
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+		"X-Amzn-Trace-Id": "Root=1-632dd138-2243642a76ba30163e857a96"
+	},
+	"json": null,
+	"origin": "113.200.214.222",
+	"url": "http://httpbin.org/put"
+}"#;
