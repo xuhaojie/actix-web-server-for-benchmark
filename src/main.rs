bec28db1800bc59a2c692e31dc61aa08b6f4ddfc
xuhaojie <xuhaojie@hotmail.com>
2022-09-27 14:45:57 +0800

    尝试改用rustls

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 // to create a self-signed temporary cert for testing: `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
 
-use actix_web::{get, web, App, HttpRequest, HttpServer, Responder,HttpResponse, http::header};
+use std::{io::BufReader, fs::File};
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, pkcs8_private_keys};
+
+use actix_web::{get, web::{self}, App, HttpRequest, HttpServer, Responder,HttpResponse, http::header};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use anyhow::{*, Result};
 use clap::{Arg,  Command};
@@ -11,11 +15,7 @@ const DEFAULT_IP : &str = "0.0.0.0";
 const DEFAULT_PORT : u16 = 3000;
 const DEFAULT_KEY_FILE : &str= "key.pem";
 const DEFAULT_CERT_FILE : &str= "cert.pem";
-
-#[get("/")]
-async fn index(_req: HttpRequest) -> impl Responder {
-    "Welcome!"
-}
+const DEFAULT_CONNECTIONS : usize = 25*1024;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
@@ -36,6 +36,7 @@ async fn main() -> Result<()> {
 							.value_name("cert")
 							.takes_value(true)
 						  	.help("Certificate chain file, default cert.pem, env key: CERT_FILE"))
+					
 						  .arg(Arg::with_name("ip")
 						  	.short('i')
 							.value_name("ip")
@@ -51,11 +52,18 @@ async fn main() -> Result<()> {
 						  	.short('s')
 							.value_name("https port")
 							.takes_value(true)
-						  	.help("Enable and specify the https server port, env key: HTTPS_PORT"));
-		
+						  	.help("Enable and specify the https server port, env key: HTTPS_PORT"))
+						  .arg(Arg::with_name("workers")
+						  	.short('w')
+							.value_name("workers")
+							.takes_value(true)
+						  	.help("Workers, default cpu core number, env key: WORKERS"))
+						  .arg(Arg::with_name("max_connections")
+						  	.short('m')
+							.value_name("max_connections")
+							.takes_value(true)
+						  	.help("Max connections, default 25k, env key: CONNECTIONS"));
 
-	let server = HttpServer::new(|| App::new().configure(benchmark_routes));
-	
 	let matches = cmd.get_matches();
 
 	let key_file = match matches.value_of("key"){
@@ -70,7 +78,7 @@ async fn main() -> Result<()> {
 		Some(file) =>	file.to_string(),
 		_ => match dotenv::var("CERT_FILE") {
 			dotenv::Result::Ok(file) => file,
-			_ => DEFAULT_KEY_FILE.to_string(),
+			_ => DEFAULT_CERT_FILE.to_string(),
 		}
 	};
 
@@ -98,6 +106,36 @@ async fn main() -> Result<()> {
 		}
 	};
 
+	let workers = match matches.value_of("workers"){
+		Some(workers) => workers.parse::<usize>()?,
+		_ => match dotenv::var("WORKERS") {
+			dotenv::Result::Ok(workers) => workers.parse::<usize>()?,
+			_ => 0,
+		}
+	};
+
+	let connections = match matches.value_of("max_connections"){
+		Some(connections) => connections.parse::<usize>()?,
+		_ => match dotenv::var("CONNECTIONS") {
+			dotenv::Result::Ok(connections) => connections.parse::<usize>()?,
+			_ => DEFAULT_CONNECTIONS,
+		}
+	};
+
+	let mut server = HttpServer::new(|| App::new()
+	.configure(server_routes)
+	.configure(benchmark_routes));
+	
+	if workers > 0 	{
+		info!("set server workers to {}", workers);
+		server = server.workers(workers);
+	}
+
+	if connections > 0 	{
+		info!("set server max connections to {}", connections);
+		server = server.max_connections(connections);
+	}
+
 	let http_address = format!("{}:{}", server_ip, http_port);
 
 	info!("http server listen on {}", http_address);
@@ -107,13 +145,29 @@ async fn main() -> Result<()> {
 	if https_port != 0 {				   
 		let https_address = format!("{}:{}", server_ip, https_port);
 		info!("https server listen on {}", https_address);
-
+/*
 		let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
 		builder.set_private_key_file(key_file, SslFiletype::PEM)?;
 	    builder.set_certificate_chain_file(cert_file)?;
 		
-		server.bind_openssl(https_address, builder)?.run().await?;
-	}else{
+		//server.bind_openssl(https_address, builder)?.run().await?;
+*/
+
+		let cert_file_ = &mut BufReader::new(File::open(cert_file)?);
+		let key_file_ = &mut BufReader::new(File::open(key_file)?);
+		
+		let cert_chain = certs(cert_file_)?.into_iter().map(Certificate).collect();
+		let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file_)?.into_iter().map(PrivateKey).collect();
+		
+		if keys.is_empty() {
+			return Err(anyhow!("Could not locate PKCS 8 private keys."));
+		}
+		
+		let config = ServerConfig::builder().with_safe_defaults().with_no_client_auth();
+		
+		server.bind_rustls(https_address, config.with_single_cert(cert_chain, keys.remove(0))?)?.run().await?;
+		//server.bind_rustls(https_address, builder)?.run().await?;
+	} else {
 		server.run().await?;
 	}
 	
@@ -121,13 +175,44 @@ async fn main() -> Result<()> {
 
 }
 
+
+pub fn server_routes(cfg: &mut web::ServiceConfig) {
+	cfg
+	.route("/", web::get().to(index));
+}
+
+pub async fn index() -> HttpResponse  {
+	HttpResponse::Ok()
+	.insert_header(header::ContentType(mime::TEXT_HTML_UTF_8))
+	.body(
+r#"<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<title>HTTP(S) Benchmark Server</title>
+	<link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+	<script src='main.js'></script>
+</head>
+<body>
+	
+	<div style="text-align:center;">
+		<H1>HTTP(S) Benchmark Server</H1>
+	</div>
+</body>
+</html>"#
+	)
+}
+
+
 pub fn benchmark_routes(cfg: &mut web::ServiceConfig) {
 	cfg
+	.route("/", web::get().to(index))
 	.route("/get", web::get().to(bench_get))
 	.route("/post", web::post().to(bench_post))
 	.route("/put", web::put().to(bench_put))
 	.route("/delete", web::delete().to(bench_delete));
 }
+
 
 pub async fn bench_get() -> HttpResponse  {
 	HttpResponse::Ok()
